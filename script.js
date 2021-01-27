8e47a6b9baae600a10084f350a8f78109a0bfb0a
AnuraghSarkar <trithatimalsina@gmail.com>
2021-01-27 10:44:04 +0545

    Javascript File

---
+++ script.js
@@ -0,0 +1,78 @@
+// Elements
+
+const quoteContainer = document.querySelector('#quote-container');
+const quoteText = document.querySelector('#quote');
+const authorText = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('.container')
+
+// SHow Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  if (!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+  }
+}
+
+
+// Quote from API
+
+async function getQuote() {
+  loading();
+  const apiUrl = 'https://goquotes-api.herokuapp.com/api/v1/random?count=1';
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    // If author is not shown
+    if (data.quotes[0].author === "") {
+      authorText.innerText = "Anonymous";
+    } else {
+      authorText.innerText = data.quotes[0].author;
+    }
+
+    // Minimize font for long quotes
+
+    if (data.quotes[0].text.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    // Stop Loader and display code
+    complete();
+    quoteText.innerText = data.quotes[0].text;
+  
+  }
+
+  catch (error) {
+    getQuote();
+    console.log("Quote dismissed", error)
+  }
+}
+
+// Tweet Quote
+
+function postTwitter() {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const tweetUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(tweetUrl, '_blank');
+}
+
+
+
+// Event Listener
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', postTwitter);
+
+
+// Onload
+getQuote();
+
